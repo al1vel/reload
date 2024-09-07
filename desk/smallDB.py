0b905aba73d618663b17bd86fa379a0fdb00e066
al1vel <m1233danil@bk.ru>
2024-09-07 03:55:25 +0300

    Added database funcs. Added func to add time to db

--- desk/smallDB.py
+++ desk/smallDB.py
@@ -11,3 +11,23 @@ def db_init():
     date TEXT NOT NULL,
     amount_of_time INTEGER,
     occupation TEXT NOT NULL)''')
+
+
+def add_time_to_db(date, amount_of_time, occupation):
+    connection = sqlite3.connect('my_db.db', check_same_thread=False)
+    cursor = connection.cursor()
+
+    cursor.execute(f'INSERT INTO Operations (date, amount_of_time, occupation) VALUES (?, ?, ?)', (date, amount_of_time, occupation))
+    connection.commit()
+    connection.close()
+
+
+def get_all_regs():
+    connection = sqlite3.connect('my_db.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Operations')
+    data = cursor.fetchall()
+    print(data)
+
+    connection.commit()
+    connection.close()
--- desk/small_main.py
+++ desk/small_main.py
@@ -1,10 +1,16 @@
 import sys
+import datetime
+import smallDB
+import sqlite3
 
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from smallUI import Ui_MainWindow
 from addTimeUI import Ui_DialogAddTime
+
+connection = sqlite3.connect('my_db.db', check_same_thread=False)
+cursor = connection.cursor()
 
 
 class TimeTracker(QMainWindow):
@@ -21,8 +27,32 @@ class TimeTracker(QMainWindow):
         self.ui_window.setupUi(self.new_window)
         self.new_window.show()
 
+        self.ui_window.buttonADD.clicked.connect(self.add_time)
+
+    def add_time(self):
+        date = datetime.date.today().strftime("%d-%m-%Y")
+        print(date)
+
+        hours_cnt = self.ui_window.lineEditHours.text()
+        print(hours_cnt)
+
+        minutes_cnt = self.ui_window.lineEditMinutes.text()
+        print(minutes_cnt)
+
+        comment = self.ui_window.textEditOccupation.toPlainText()
+        print(comment)
+
+        amount_of_time = int(hours_cnt) * 60 + int(minutes_cnt)
+        print(amount_of_time)
+
+        smallDB.add_time_to_db(date, amount_of_time, comment)
+        smallDB.get_all_regs()
+        self.new_window.close()
+
 
 if __name__ == "__main__":
+    smallDB.db_init()
+
     app = QApplication(sys.argv)
     window = TimeTracker()
     window.show()
